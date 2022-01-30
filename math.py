393f1731ab28e2b12ec9940d4b691bec3456097f
Priyanshi Gupta <86509977+Priyanshii16@users.noreply.github.com>
2022-01-30 20:07:39 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add implementation
+def add(x,y):
+    return x+y
+#Subtract implementation
+def subtract(x,y):
+    return x-y       #on master branch
+#Multiply implementation
+def multiply(x,y):
+    return x*y   #on bug456 branch
+#Divide implementation
+def divide(x,y):   #on master branch
+    if y==0:
+        returb Divide_by_zero_error
+    else:
+        return x/y 
+
+#square implementation
+def square(x):
+    return x*x
